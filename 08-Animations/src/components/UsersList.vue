db0ccd06b0ecc3a23ef49433b3acfc570b5fe370
Vivek <vivek9142@gmail.com>
2022-09-23 02:56:14 +0530

    added page components and users-list files

---
+++ 08-Animations/src/components/UsersList.vue
@@ -0,0 +1,80 @@
+<template>
+    <!-- <ul> -->
+        <transition-group tag="ul" name="user-list">
+            <li v-for="user in users" @click="removeUser(user)" :key="user">
+            {{user}}
+            </li>
+        </transition-group>
+    <!-- </ul> -->
+    <div>
+        <input type="text" ref="userName" />
+        <button @click="addUser">Add User</button>
+    </div>
+</template>
+
+<script>
+export default {
+    methods:{
+        addUser(){
+            const enteredUser = this.$refs.userName.value;
+            this.users.unshift(enteredUser);
+        },
+        removeUser(user){
+            this.users = this.users.filter(usr => usr !== user );
+        }
+    },
+    data(){
+        return {
+            users: ['Max','Manu','Angela','Michael'],
+        }
+    }
+}
+</script>
+
+<style scoped>
+ul{
+    list-style: none;
+    margin: 1rem 0;
+    padding: 0;
+}
+
+li{
+    border: 1px solid #ccc;
+    padding: 1rem;
+    text-align: center;
+}
+
+.user-list-enter-from {
+    opacity: 0;
+    transform: translateX(-30px);
+}
+
+.user-list-enter-active {
+    transition: all 1s ease-out;
+}
+
+.user-list-enter-to {
+    opacity: 1;
+    transform: translateX(0);
+}
+
+.user-list-leave-from {
+    opacity: 1;
+    transform: translateX(0);
+}
+
+.user-list-leave-active {
+    transition: all 1s ease-in;
+    position:absolute;
+}
+
+.user-list-leave-to {
+    opacity: 0;
+    transform: translateX(30px);
+}
+
+.user-list-move {
+    transition: transform 0.8s ease;
+}
+
+</style>
