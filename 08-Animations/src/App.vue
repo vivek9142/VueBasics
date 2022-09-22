46bba39eb52a4e01ad69d99e6792ba7f0ca5cf6d
Vivek <vivek9142@gmail.com>
2022-09-23 02:52:34 +0530

    added animating route changes, comments in app.vue,main.js

--- 08-Animations/src/App.vue
+++ 08-Animations/src/App.vue
@@ -1,5 +1,73 @@
+1 -There's one specific kind of thing which you might want to navigate,
+which we haven't seen thus far though. And that would be changes between two routes.
+
+For that, I'll quickly add routing here, simply by adding a new pages folder
+
+2 - but we actually see that router-view can no longer be used directly inside of transition.
+Now, it says no longer, because previously with older versions of Vue, that did work.
+So maybe if you learned that, you are still used to this old way of doing it.
+
+With Vue 3 this is no longer possible. And we actually get the solution
+here in the warning as well. We need to restructure our code. Transition now needs to go inside of 
+our router-view.
+
+So we should move transition inside there. But this alone now all the won't do it
+because while router-view supports getting content that will be rendered inside of it,
+now we also need to tell it where to actually render the actual page,
+the actual component that should be rendered based on the route that is active.
+
+And for that, we can use the scoped slots syntax router-view uses under the hood.
+Normally we don't need it, but for transitions with routes, you do need it.
+You can use this v-slot directive, which I introduced earlier in the course when we talked about slots,
+to extract the slotProps you're getting here. You get those by the router-view component,
+which is built in. And you can then use the dynamic component component
+to bind it to slotProps.Component.
+
+So slotProps have a property named Component with a capital C, which holds the component
+that should be loaded for the currently selected route. And this strange looking syntax is what 
+you need to animate switches between these components.
+
+3 - And now this kind of works, but of course this looks really stupid here, right?
+I mean, it's just moving around and we got two components on the page at the same time.
+So this is not fully what we want, but technically this is correct at least.
+But one thing we want to change here is something we already saw earlier
+when we switched between two buttons. We want to make sure that only one component
+is shown on the page at the same time.
+
+And you might remember that we can achieve this by adding the mode prop to the transition component.
+And there we add out-in and in-out. And if we use out-in, we ensure that first,
+the leaving component is animated before then the entering component is animated.
+
+4 -  one thing you might have noticed before is that when you used that other animation
+which looked a bit strange, the route animation here, we even saw that animation when we loaded the page
+for the first time.
+
+The reason for data's that with the vue-router, technically the first act of route is an empty route.
+And then when your page starts, when your page is loaded for the first time,
+it immediately switches to the route that should be used for the given path in your URL.
+That means that technically when the app starts, the vue-router goes from no route
+to the chosen route for to given path. And hence it plays an animation
+because we go from one component, nothing, to the to be loaded component.
+
+To avoid this behavior, you can go to main.js
+
+
 <template>
-  <div class="container">
+  <!-- 1-adding router-view and transition and its classes -->
+  <!-- <transition name="route">
+    <router-view></router-view>
+  </transition> -->
+
+  <!-- 2- changing router-transition setup-->
+  <router-view v-slot="slotProps">
+    <!-- 3- mode part -->
+    <transition name="route" mode="out-in">
+    <!-- <transition name="fade-button" mode="out-in"> -->
+        <component :is="slotProps.Component"></component>
+    </transition>
+  </router-view>
+
+  <!-- <div class="container">
     <UsersList/>
   </div>
   <div class="container">
@@ -40,14 +108,14 @@
   </base-modal>
   <div class="container">
     <button @click="showDialog">Show Dialog</button>
-  </div>
+  </div> -->
 </template>  
 
 <script>
-import UsersList from './components/UsersList.vue';
+// import UsersList from './components/UsersList.vue';
 
 export default {
-  components:{UsersList},
+  // components:{UsersList},
   data() {
     return { 
     animatedBlock:false,
@@ -223,7 +291,15 @@ button:active {
   animation: slide-fade 0.3s ease-out forwards;
 }
 
+.route-enter-from {}
+.route-enter-active {
+  animation: slide-scale 0.4s ease-in;
+}
+.route-enter-to {}
 
+.route-leave-active {
+  animation: slide-scale 0.4s ease-in;
+}
 
 @keyframes slide-scale {
   0% {
--- 08-Animations/src/main.js
+++ 08-Animations/src/main.js
@@ -1,10 +1,43 @@
 import { createApp } from 'vue';
+import { createRouter, createWebHistory } from 'vue-router';
 
 import App from './App.vue';
 import BaseModal from './components/BaseModal.vue';
 
+import AllUsers from './pages/AllUsers.vue';
+import CourseGoals from './pages/CourseGoals.vue';
+
 const app = createApp(App);
+// 1- added router , paths ,pages , commented rest of code in app.vue and
+//added router-view,transition
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    { path: '/', component: AllUsers },
+    { path: '/goals', component: CourseGoals },
+  ],
+});
+
+app.use(router);
 
 app.component('base-modal', BaseModal);
 
-app.mount('#app');
+/*
+4- go where you register the router and there you can actually use the router to call the is ready method on it. 
+This method will let you know when the router successfully evaluated which page should be loaded.
+This gives you a promise and hence you can call then on it. And in the function you pass to this then block,
+in this function the router will now not go from the empty to the to be selected page.
+
+But instead, at this point of time, it is aware of the page that should be loaded
+and it will therefore not make this initial transition. So you can then mount your app inside of this 
+function to only mount and therefore render your app once the router is fully set up and aware of 
+the page that should be loaded.
+
+And if you do that and reload, you don't have this initial animation.
+*/
+
+router.isReady().then(function () {
+  app.mount('#app');
+});
+
+// app.mount('#app');
