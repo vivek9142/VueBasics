7dbfea3a3a2f1c15f50f73807023359cf8f8d7ef
Vivek <vivek9142@gmail.com>
2022-09-24 00:07:52 +0530

    added basic counter state mgmnt

--- 09-Vuex-state-mgmnt/src/App.vue
+++ 09-Vuex-state-mgmnt/src/App.vue
@@ -1,14 +1,43 @@
 <template>
-  <base-container title="Vuex"></base-container>
+  <base-container title="Vuex">
+
+    <!-- 
+      there now is a new property, which we can access in every component.
+      And that's the dollar sign store property, which points at the vuex store.
+      This then has a state property, which points at the state managed by the 
+      store. 
+      
+      we can also store this value in computed function property
+      -->
+    <!-- <h3>{{$store.state.counter}}</h3> -->
+    <!-- <h3> {{ counter }}</h3> -->
+    
+    <TheCounter/>
+    <button @click="addOne">Add 1</button>
+  </base-container>
 </template>
 
 <script>
 import BaseContainer from './components/BaseContainer.vue';
+//register TheCounter here
+import TheCounter from './components/TheCounter.vue';
 
 export default {
   components: {
     BaseContainer,
+    TheCounter
   },
+  //copy this value and h3 tag into theCounter comp
+  // computed:{
+  //   counter(){
+  //     return this.$store.state.counter;
+  //   }
+  // },
+  methods:{
+    addOne(){
+      this.$store.state.counter++;
+    }
+  }
 };
 </script>
 
--- 09-Vuex-state-mgmnt/src/main.js
+++ 09-Vuex-state-mgmnt/src/main.js
@@ -1,7 +1,20 @@
 import { createApp } from 'vue';
+import { createStore } from 'vuex';
 
 import App from './App.vue';
 
+//createStore to create global store in app
+const store = createStore({
+  state() {
+    // this object will hold the application-wide data
+    return {
+      counter: 0,
+    };
+  },
+});
+
 const app = createApp(App);
+
+app.use(store);
 
 app.mount('#app');
