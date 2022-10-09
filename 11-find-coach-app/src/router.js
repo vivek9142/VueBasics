99d830035684c729b4f9e9d7f80a9e90fcf657a7
Vivek <vivek9142@gmail.com>
2022-10-09 16:02:27 +0530

    added app comp structure, completed app router configuration

--- 11-find-coach-app/src/router.js
+++ 11-find-coach-app/src/router.js
@@ -1,17 +1,27 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
+import CoachesDetail from './pages/coaches/CoachesDetail.vue';
+import CoachesList from './pages/coaches/CoachList.vue';
+import CoachRegistration from './pages/coaches/CoachRegistration.vue';
+
+import ContactCoach from './pages/requests/ContactCoach.vue';
+import RequestsReceived from './pages/requests/RequestsReceived.vue';
+
+import NotFound from './pages/NotFound.vue';
+
 const router = createRouter({
   history: createWebHistory(),
   routes: [
-    { path: '/coaches', component: null },
+    { path: '/', redirect: '/coaches' },
+    { path: '/coaches', component: CoachesList },
     {
       path: '/coaches/:id',
-      component: null,
-      children: [{ path: 'contact', component: null }], // /coaches/c1/contact
+      component: CoachesDetail,
+      children: [{ path: 'contact', component: ContactCoach }], // /coaches/c1/contact
     },
-    { path: '/register', component: null },
-    { path: '/requests', component: null },
-    { path: '/:notFound(.*)', component: null },
+    { path: '/register', component: CoachRegistration },
+    { path: '/requests', component: RequestsReceived },
+    { path: '/:notFound(.*)', component: NotFound },
   ],
 });
 
