90b68300143903788b126a1a4102a421c4a5ea25
Vivek <vivek9142@gmail.com>
2022-10-09 15:43:05 +0530

    added routes config and installed vuex,vue-rrouter

---
+++ 11-find-coach-app/src/router.js
@@ -0,0 +1,18 @@
+import { createRouter, createWebHistory } from 'vue-router';
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    { path: '/coaches', component: null },
+    {
+      path: '/coaches/:id',
+      component: null,
+      children: [{ path: 'contact', component: null }], // /coaches/c1/contact
+    },
+    { path: '/register', component: null },
+    { path: '/requests', component: null },
+    { path: '/:notFound(.*)', component: null },
+  ],
+});
+
+export default router;
